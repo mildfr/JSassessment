ea52ab0a5fa67938addaced73a3718b3f313ff0f
Saurabh <106650017+mildfr@users.noreply.github.com>
2023-08-05 17:59:11 +0530

    Update JStest.js

--- JStest.js
+++ JStest.js
@@ -9,43 +9,59 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
-const NFTs = []
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT (_name, _eyeColor, _shirtType, _bling) {
-    const NFT = {
-        "name": _name,
-        "eyeColor": _eyeColor,
-        "shirtType": _shirtType,
-        "bling": _bling
-    }
-    NFTs.push(NFT);
-    console.log("Minted: " + _name);
-}
+//function mintNFT () {
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
-function listNFTs () {
- for(let i = 0; i < NFTs.length; i++){
-        console.log("\nID: \t\t" + (i+1));
-        console.log("Name: \t\t" + NFTs[i].name);
-        console.log("Eyecolor: \t" + NFTs[i].Eyecolor);
-        console.log("Shirt Type: " + NFTs[i].ShirtType);
-        console.log("Bling \t\t" + NFTs[i].bling);
- }
-}
+//function listNFTs () {
 
 // print the total number of NFTs we have minted to the console
-function getTotalSupply() {
- console.log("\n" + NFTs.length);
-}
+//function getTotalSupply() {
 
 // call your functions below this line
-mintNFT("Bob", "Blue", "Hoodie", "Gold chain");
-mintNFT("Sue", "Blue", "Hoodie", "Gold chain");
-mintNFT("Tim", "Blue", "Hoodie", "Gold chain");
-mintNFT("Jogn", "Blue", "Hoodie", "Gold chain");
-listNFTs();
-getTotalSupply();
+
+
+// Step 1: Create an array variable to hold the NFTs
+let nfts = [];
+
+// Step 2: Create an object inside mintNFT function to hold the metadata
+function mintNFT(_name, _city, _profession, _organisation) {
+  let nft = {
+    name: _name,
+    city: _city,
+    profession: _profession,
+    organisation: _organisation,
+  };
+
+  // Step 3: Store the NFT in the variable
+  nfts.push(nft);
+}
+
+// Step 4: Function to list all NFTs' metadata
+function listNFTs() {
+  nfts.forEach((nft, index) => {
+    console.log("NFT           " + (index + 1));
+    console.log("Name:         " + nft.name);
+    console.log("City:         " + nft.city)
+    console.log("Profession:   " + nft.profession);
+    console.log("Organisation: " + nft.organisation);
+    console.log("__________________________________");
+  });
+}
+
+// Step 5: Function to get the total supply of NFTs
+function getTotalSupply() {
+  return nfts.length;
+}
+
+
+//example
+mintNFT("Chris","Amsterdam","Software developer","Metacrafters");
+mintNFT("Sam","Berlin","Graphic designer","Microsoft");
+mintNFT("Max","Moscow","Manager","Google");
+listNFTs();     //It will print all the metadata
+console.log("Total NFTs: " + getTotalSupply()); // To get the total supply of NFTs
